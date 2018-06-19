814895536ed3853004d862d259d1f1a39b87141a
ramkashyap17 <ramkashyap17@gmail.com>
2018-06-19 23:04:46 +0530

    #4 Convert bootstrap app into a React app; #3 Create a react component for the LHS pane

--- workspace/public/js/main.js
+++ workspace/public/js/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {        
+// class Left extends React.Component {
+// 	render() {
+// 		return 
+// 			<div className="container left">
+// 				<button class="btn btn-right">CREATE</button>
+// 				<br/>
+// 				<br/>
+// 			</div>	
+// 	}
+// }
 
-});
+// ReactDOM.render(
+// 	<h1>Kanban Board</h1>,
+// 	<Left />,
+// 	document.getElementById('rootElement')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
