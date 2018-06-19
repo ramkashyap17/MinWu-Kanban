0686587cb1cae157196a383eea35353b2ed4778c
ramkashyap17 <ramkashyap17@gmail.com>
2018-06-19 10:03:00 +0530

    Initial commit from = Min Wu

---
+++ workspace/app/controllers/kanban.card.controller.js
@@ -0,0 +1,102 @@
+var Card = require('../models/kanban.card.model');
+
+var list = function(req, res) {
+	var by = req.query.by;
+	Card.find({}, by, function(err, cards) {
+		if (err) {
+			res.status(400).json({error: err.message});
+		} else {
+			res.json(cards);
+		}
+	});
+};
+
+var create = function(req, res) {
+	var card = new Card();
+	card.title = req.body.title;
+	card.description = req.body.description;
+	card.status = req.body.status;
+
+	card.save(function(err) {
+		if (err) {
+			res.status(400).json({error: err.message});
+		} else {
+			res.status(201).json(card);
+		}
+	});
+};
+
+var read = function(req, res) {
+	setTimeout(function() {
+		Card.findById(req.params.card_id, function(err, card) {
+			if (err) {
+				res.status(404).json({error: err.message});
+			} else {
+				res.json(card);
+			}
+		});
+	}, 3000)
+};
+
+var update = function(req, res) {
+	Card.findById(req.params.card_id, function(err, card) {
+		if (err) {
+			res.status(404).json({error: err.message});
+		} else {
+			if (card !== null) {
+				card.title = req.body.title;
+				card.description = req.body.description;
+				card.status = req.body.status;
+
+				card.save(function(err) {
+					if (err) {
+						res.status(400).json({error: err.message});
+					} else {
+						res.json(card);
+					}
+				});
+			} else {
+				res.status(404).json({error: 'card not found'});
+			}
+		}
+	});
+};
+
+var remove = function(req, res) {
+	Card.findById(req.params.card_id, function(err, card) {
+		if (err) {
+			res.status(404).json({error: err.message});
+		} else {
+			if (card !== null) {
+				card.remove(function(err) {
+					if (err) {
+						res.status(400).json({error: err.message});
+					} else {
+						res.status(204).json({confirmation: 'success'});
+					}
+				});
+			} else {
+				res.status(404).json({error: 'card not found'});
+			}
+		}
+	});
+};
+
+var removeAll = function(req, res) {
+	Card.remove({}, function(err) {
+		if (err) {
+			res.status(400).json({error: err.message});
+		} else {
+			res.status(204).json({confirmation: 'success'});
+		}
+	});
+};
+
+module.exports = {
+	list: list,
+	create: create,
+	read: read,
+	update: update,
+	remove: remove,
+	removeAll: removeAll
+};
